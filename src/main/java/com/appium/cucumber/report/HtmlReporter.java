70d0f55f4817829820cc9e1ebdcdcb4e4a34679e
Krishnan, K. S <k.s.krishnan@accenture.com>
2016-09-15 16:18:02 +0530

    Modified APK & IPA paths to be passed as absolute paths

--- src/main/java/com/appium/cucumber/report/HtmlReporter.java
+++ src/main/java/com/appium/cucumber/report/HtmlReporter.java
@@ -1,51 +1,52 @@
 package com.appium.cucumber.report;
 
-import net.masterthought.cucumber.Configuration;
-import net.masterthought.cucumber.ReportBuilder;
-import org.apache.velocity.exception.VelocityException;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.velocity.exception.VelocityException;
+
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
 
 public class HtmlReporter {
     public File reportOutputDirectory = new File(System.getProperty("user.dir") + "/target/");
     public List<String> list = new ArrayList<String>();
 
     public void listFilesForFolder(final File folder) {
-        for (final File fileEntry : folder.listFiles()) {
-            if (fileEntry.isDirectory()) {
-                listFilesForFolder(fileEntry);
-            } else if (fileEntry.getName().endsWith(".json")) {
-                System.out.println("*******" + fileEntry.getName());
-                list.add(reportOutputDirectory + "/" + fileEntry.getName());
+	for (final File fileEntry : folder.listFiles()) {
+	    if (fileEntry.isDirectory()) {
+		listFilesForFolder(fileEntry);
+	    } else if (fileEntry.getName().endsWith(".json")) {
+		System.out.println("*******" + fileEntry.getName());
+		list.add(reportOutputDirectory + "/" + fileEntry.getName());
 
-            }
-        }
+	    }
+	}
     }
 
     public void generateReports() throws VelocityException, IOException {
-        listFilesForFolder(reportOutputDirectory);
-        String jenkinsBasePath = "";
-        String buildNumber = "1";
-        String projectName = "cucumber-jvm";
-        boolean skippedFails = true;
-        boolean pendingFails = false;
-        boolean undefinedFails = true;
-        boolean missingFails = true;
-        boolean runWithJenkins = false;
-        boolean parallelTesting = false;
+	listFilesForFolder(reportOutputDirectory);
+	String jenkinsBasePath = "";
+	String buildNumber = "1";
+	String projectName = "cucumber-jvm";
+	boolean skippedFails = true;
+	boolean pendingFails = false;
+	boolean undefinedFails = true;
+	// boolean missingFails = true; - Not used
+	boolean runWithJenkins = false;
+	boolean parallelTesting = false;
 
-        Configuration configuration = new Configuration(reportOutputDirectory, projectName);
-        configuration.setStatusFlags(skippedFails, pendingFails, undefinedFails, missingFails);
-        configuration.setParallelTesting(parallelTesting);
-        configuration.setJenkinsBasePath(jenkinsBasePath);
-        configuration.setRunWithJenkins(runWithJenkins);
-        configuration.setBuildNumber(buildNumber);
+	Configuration configuration = new Configuration(reportOutputDirectory, projectName);
+	configuration.setStatusFlags(skippedFails, pendingFails, undefinedFails);
+	configuration.setParallelTesting(parallelTesting);
+	configuration.setJenkinsBasePath(jenkinsBasePath);
+	configuration.setRunWithJenkins(runWithJenkins);
+	configuration.setBuildNumber(buildNumber);
 
-        ReportBuilder reportBuilder = new ReportBuilder(list, configuration);
-        reportBuilder.generateReports();
+	ReportBuilder reportBuilder = new ReportBuilder(list, configuration);
+	reportBuilder.generateReports();
     }
 
 }
--- src/test/java/com/test/site/UserBaseTest.java
+++ src/test/java/com/test/site/UserBaseTest.java
@@ -1,11 +1,9 @@
 package com.test.site;
 
-import com.appium.manager.AppiumParallelTest;
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.remote.AndroidMobileCapabilityType;
-import io.appium.java_client.remote.IOSMobileCapabilityType;
-import io.appium.java_client.remote.MobileCapabilityType;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+
 import org.json.simple.JSONObject;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.ITestResult;
@@ -14,72 +12,78 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
-import java.io.IOException;
-import java.lang.reflect.Method;
+import com.appium.manager.AppiumParallelTest;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.IOSMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
 
 public class UserBaseTest extends AppiumParallelTest {
     JSonParser jSonParser = new JSonParser();
 
-    @BeforeMethod() public void startApp(Method name) throws Exception {
-        driver = startAppiumServerInParallel(name.getName(), androidNative());
-        startLogResults(name.getName());
+    @BeforeMethod()
+    public void startApp(Method name) throws Exception {
+	driver = startAppiumServerInParallel(name.getName(), androidNative());
+	startLogResults(name.getName());
     }
 
-    @AfterMethod() public void killServer(ITestResult result)
-        throws InterruptedException, IOException {
-        endLogTestResults(result);
-        getDriver().quit();
-        //deleteAppIOS("com.tesco.sample");
+    @AfterMethod()
+    public void killServer(ITestResult result) throws InterruptedException, IOException {
+	endLogTestResults(result);
+	getDriver().quit();
+	// deleteAppIOS("com.tesco.sample");
     }
 
     public AppiumDriver<MobileElement> getDriver() {
-        return driver;
+	return driver;
     }
 
-    @BeforeClass() public void beforeClass() throws Exception {
-        System.out.println("Before Class called" + Thread.currentThread().getId());
-        System.out.println(getClass().getName());
-        startAppiumServer(getClass().getSimpleName());
+    @BeforeClass()
+    public void beforeClass() throws Exception {
+	System.out.println("Before Class called" + Thread.currentThread().getId());
+	System.out.println(getClass().getName());
+	startAppiumServer(getClass().getSimpleName());
     }
 
-    @AfterClass() public void afterClass() throws InterruptedException, IOException {
-        System.out.println("After Class" + Thread.currentThread().getId());
-        killAppiumServer();
+    @AfterClass()
+    public void afterClass() throws InterruptedException, IOException {
+	System.out.println("After Class" + Thread.currentThread().getId());
+	killAppiumServer();
     }
 
     public void getUserName() {
-        String[] crds = Thread.currentThread().getName().toString().split("_");
-        System.out.println(crds[1]);
-        JSONObject user = jSonParser.getUserData(Integer.parseInt(crds[1]));
-        System.out.println(user.get("userName"));
+	String[] crds = Thread.currentThread().getName().toString().split("_");
+	System.out.println(crds[1]);
+	JSONObject user = jSonParser.getUserData(Integer.parseInt(crds[1]));
+	System.out.println(user.get("userName"));
     }
 
     public DesiredCapabilities iosNative() {
-        DesiredCapabilities iOSCapabilities = new DesiredCapabilities();
-        System.out.println("Setting iOS Desired Capabilities:");
-        iOSCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9.0");
-        iOSCapabilities.setCapability(MobileCapabilityType.APP, prop.getProperty("IOS_APP_PATH"));
-        iOSCapabilities
-            .setCapability(IOSMobileCapabilityType.BUNDLE_ID, prop.getProperty("BUNDLE_ID"));
-        iOSCapabilities.setCapability(IOSMobileCapabilityType.AUTO_ACCEPT_ALERTS, true);
-        iOSCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone");
-        iOSCapabilities.setCapability(MobileCapabilityType.UDID, device_udid);
-        return iOSCapabilities;
+	DesiredCapabilities iOSCapabilities = new DesiredCapabilities();
+	System.out.println("Setting iOS Desired Capabilities:");
+	iOSCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9.0");
+	iOSCapabilities.setCapability(MobileCapabilityType.APP,
+		new File(prop.getProperty("IOS_APP_PATH")).getAbsolutePath());
+	iOSCapabilities.setCapability(IOSMobileCapabilityType.BUNDLE_ID, prop.getProperty("BUNDLE_ID"));
+	iOSCapabilities.setCapability(IOSMobileCapabilityType.AUTO_ACCEPT_ALERTS, true);
+	iOSCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone");
+	iOSCapabilities.setCapability(MobileCapabilityType.UDID, device_udid);
+	return iOSCapabilities;
     }
 
     public synchronized DesiredCapabilities androidNative() {
-        System.out.println("Setting Android Desired Capabilities:");
-        DesiredCapabilities androidCapabilities = new DesiredCapabilities();
-        androidCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android");
-        androidCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "5.X");
-        androidCapabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY,
-            prop.getProperty("APP_ACTIVITY"));
-        androidCapabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE,
-            prop.getProperty("APP_PACKAGE"));
-        androidCapabilities.setCapability("browserName", "");
-        androidCapabilities
-            .setCapability(MobileCapabilityType.APP, prop.getProperty("ANDROID_APP_PATH"));
-        androidCapabilities.setCapability(MobileCapabilityType.UDID, device_udid);
-        return androidCapabilities;
+	System.out.println("Setting Android Desired Capabilities:");
+	DesiredCapabilities androidCapabilities = new DesiredCapabilities();
+	androidCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android");
+	androidCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "5.X");
+	androidCapabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, prop.getProperty("APP_ACTIVITY"));
+	androidCapabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, prop.getProperty("APP_PACKAGE"));
+	androidCapabilities.setCapability("browserName", "");
+	androidCapabilities.setCapability(MobileCapabilityType.APP,
+		new File(prop.getProperty("ANDROID_APP_PATH")).getAbsolutePath());
+	androidCapabilities.setCapability(MobileCapabilityType.UDID, device_udid);
+	return androidCapabilities;
     }
 }
